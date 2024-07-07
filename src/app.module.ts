1007af923640de667119d41801d3352adaeb0656
Sergio Rodas <sergiorodasgonzalez@gmail.com>
2024-07-06 21:28:07 -0300

    build: add lint rules and run lint across the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TaskModule } from './tasks/tasks.module';
-import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TaskModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TaskModule, ProjectsModule, AuthModule, UsersModule],
+    imports: [TaskModule, ProjectsModule, AuthModule, UsersModule],
 })
 export class AppModule {}
